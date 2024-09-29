fed190e7aa5dfc0910e3844ca7b5e656b3252ed9
Imesh Gunasekara <imesh@jrcsoftware.com>
2024-09-29 12:57:25 +0800

    script final changes

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 function updateClock() {
   const clockElement = document.getElementById('clock');
   const now = new Date();
-  console.log(now);
+  //   console.log(now);
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  //   console.log(hours + '' + minutes + '' + seconds);
+  let ampm = hours >= 12 ? 'PM' : 'AM';
+  //   console.log(ampm);
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  const timeString = `${hours}:${minutes}:${seconds} ${ampm}`;
+  clockElement.innerText = timeString;
 }
+
+setInterval(updateClock, 1000);
 
 updateClock();
